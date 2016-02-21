c80c49abb41ecd22383e35993b080254b459546c
David Wright <davidwright66@goog;email.com>
2016-02-21 20:07:56 +0000

    Added skeleton tests for ContactImpl

---
+++ tests/ContactImplTest.java
@@ -0,0 +1,35 @@
+import org.junit.After;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by davidwright on 21/02/2016.
+ */
+public class ContactImplTest {
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNotes() throws Exception {
+
+    }
+
+    @Test
+    public void testAddNotes() throws Exception {
+
+    }
+}
